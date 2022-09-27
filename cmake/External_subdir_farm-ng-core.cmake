72687513f80f4b27dd20cd77d5874a326cf3cb29
Ethan Rublee <ethan.rublee@gmail.com>
2022-09-26 18:22:34 -0700

    move cmake to module.

--- cmake/External_subdir_farm-ng-core.cmake
+++
@@ -1,8 +0,0 @@
-ExternalProject_Add(farm-ng-core
-    DEPENDS fmt expected eigen
-    PREFIX ${farm_ng_EXT_PREFIX}
-    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/farm-ng-core
-    BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/farm-ng-core-build
-    CMAKE_ARGS ${farm_ng_DEFAULT_ARGS} -DBUILD_FARM_NG_PROTOS=On
-    TEST_BEFORE_INSTALL ON
-)

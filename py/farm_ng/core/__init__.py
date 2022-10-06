0cbca145728fda5af3d5a892597569f4b87a7a14
edgar <edgar.riba@gmail.com>
2022-10-06 18:29:22 +0200

    set version for farm_ng_core

--- py/farm_ng/core/__init__.py
+++ py/farm_ng/core/__init__.py
@@ -6,4 +6,4 @@ if sys.version_info >= (3, 8):  # pragma: >=3.8 cover
 else:  # pragma: <3.8 cover
     import importlib_metadata
 
-__version__ = importlib_metadata.version("farm_ng")
+__version__ = importlib_metadata.version("farm_ng_core")

90dd8bd482391dd002ac89ddea4c9e675a4331da
edgar <edgar.riba@gmail.com>
2022-10-06 15:32:19 +0200

    remove init

--- py/farm_ng/__init__.py
+++
@@ -1,9 +0,0 @@
-# Version variable
-import sys
-
-if sys.version_info >= (3, 8):  # pragma: >=3.8 cover
-    import importlib.metadata as importlib_metadata
-else:  # pragma: <3.8 cover
-    import importlib_metadata
-
-__version__ = importlib_metadata.version("farm_ng")

c55f4a25d01dac9222bb728fd294537e625fafb2
Ethan Rublee <ethan.rublee@gmail.com>
2022-10-23 09:43:30 -0700

    try again.

--- py/farm_ng/core/package.py
+++ py/farm_ng/core/package.py
@@ -1,15 +1,10 @@
 import os
-import shutil
 import sys
 from pathlib import Path
-
-from setuptools import Command
-from setuptools.command.develop import develop
-from setuptools.command.egg_info import egg_info
-from setuptools.command.install import install
+import stat
 
 
-def build_package_protos(package_root, proto_includes=[]):
+def build_package_protos(proto_root, package_root, proto_includes=[]):
     """
     includes: additional include paths
     """
@@ -17,7 +12,9 @@ def build_package_protos(package_root, proto_includes=[]):
     import pkg_resources
 
     proto_files = []
-    inclusion_root = os.path.abspath(package_root)
+    inclusion_root = os.path.abspath(proto_root)
+    package_root = os.path.abspath(package_root)
+
     for root, _, files in os.walk(inclusion_root):
         for filename in files:
             if filename.endswith(".proto"):
@@ -43,96 +40,31 @@ def build_package_protos(package_root, proto_includes=[]):
             ]
             + includes
             + [
-                "--python_out={}".format(inclusion_root),
-                "--grpc_python_out={}".format(inclusion_root),
+                "--python_out={}".format(package_root),
+                "--grpc_python_out={}".format(package_root),
             ]
             + [proto_file]
         )
         if protoc.main(command) != 0:
             raise Exception("error: {} failed".format(command))
 
-
-class BuildProtosCommand(Command):
-    user_options = []  # type: ignore
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        proto_files_root = Path("protos")
-        build_package_protos(proto_files_root)
-
-        for proto_def in proto_files_root.rglob("*.proto"):
-            proto_def_new = Path("py", *proto_def.parts[1:])
-            shutil.copy(proto_def, proto_def_new)
-
-        for proto_file in proto_files_root.rglob("*_pb2*.py"):
-            proto_file_new = Path("py", *proto_file.parts[1:])
-            if not proto_file_new.exists():
-                proto_file.rename(proto_file_new)
-            if proto_file.exists():
-                proto_file.unlink()
-        for proto_file in proto_files_root.rglob("*_pb2*.pyi"):
-            proto_file_new = Path("py", *proto_file.parts[1:])
-            if not proto_file_new.exists():
-                proto_file.rename(proto_file_new)
-            if proto_file.exists():
-                proto_file.unlink()
+        proto_file_new = package_root / Path(proto_file).relative_to(inclusion_root)
+        Path(proto_file_new).parent.mkdir(parents=True, exist_ok=True)
+        with open(
+            proto_file_new,
+            "wt",
+        ) as proto_out:
+            proto_out.write("// This file generated for packaging, do not edit.\n")
+            proto_out.write(f"// To edit, see original source:\n  {proto_file}.\n")
+            proto_out.write(f"//\n")
+            with open(proto_file, "rt") as proto_in:
+                proto_out.write(proto_in.read())
 
 
-class CleanFilesCommand(Command):
-    user_options = []  # type: ignore
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        proto_files_root = Path("py/farm_ng")
-        for proto_def in proto_files_root.rglob("*.proto"):
-            assert proto_def.unlink() is None
-        for proto_file in proto_files_root.rglob("*_pb2*.py"):
-            assert proto_file.unlink() is None
-        for proto_file in proto_files_root.rglob("*_pb2*.pyi"):
-            assert proto_file.unlink() is None
-
-
-class BuildProtosInstall(install):
-    def run(self):
-        # 1. Build the protobufs
-        BuildProtosCommand.run(self)
-        # 2. Run the installation
-        install.run(self)
-        # 3. Clean the generated protobufs
-        CleanFilesCommand.run(self)
-
-
-class BuildProtosDevelop(develop):
-    def run(self):
-        # 1. Build the protobufs
-        BuildProtosCommand.run(self)
-        # 2. Run the installation
-        develop.run(self)
-
-
-class BuildProtosEggInfo(egg_info):
-    def run(self):
-        # 1. Build the protobufs
-        BuildProtosCommand.run(self)
-        # 2. Run the installation
-        egg_info.run(self)
-
-
-def makeCmdClass():
-    return {
-        "build_package_protos": BuildProtosCommand,
-        "install": BuildProtosInstall,
-        "develop": BuildProtosDevelop,
-        "egg_info": BuildProtosEggInfo,
-        "clean": CleanFilesCommand,
-    }
+def clean_protos(package_root):
+    for proto_def in package_root.rglob("*.proto"):
+        assert proto_def.unlink() is None
+    for proto_file in package_root.rglob("*_pb2*.py"):
+        assert proto_file.unlink() is None
+    for proto_file in package_root.rglob("*_pb2*.pyi"):
+        assert proto_file.unlink() is None
--- setup.py
+++ setup.py
@@ -2,13 +2,81 @@ from setuptools import setup
 
 # hack for bootstrapping and include farm_ng.core.package from it's setup.py
 import pathlib
+from pathlib import Path
+
+from setuptools import Command
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+from setuptools.command.install import install
 
 farm_ng_core_path = pathlib.Path(__file__).parent.resolve() / "py"
 import sys
 
 sys.path.insert(0, str(farm_ng_core_path))
 
-# normal farm_ng packages will just do the following, assuming they depend on farm_ng.core
-from farm_ng.core.package import makeCmdClass
 
-setup(cmdclass=makeCmdClass())
+class BuildProtosCommand(Command):
+    user_options = []  # type: ignore
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from farm_ng.core.package import build_package_protos
+
+        build_package_protos(proto_root=Path("protos"), package_root=Path("py"))
+
+
+class CleanFilesCommand(Command):
+    user_options = []  # type: ignore
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from farm_ng.core.package import clean_protos
+
+        clean_protos(package_root=Path("py"))
+
+
+class BuildProtosInstall(install):
+    def run(self):
+        # 1. Build the protobufs
+        BuildProtosCommand.run(self)
+        # 2. Run the installation
+        install.run(self)
+        # 3. Clean the generated protobufs
+        CleanFilesCommand.run(self)
+
+
+class BuildProtosDevelop(develop):
+    def run(self):
+        # 1. Build the protobufs
+        BuildProtosCommand.run(self)
+        # 2. Run the installation
+        develop.run(self)
+
+
+class BuildProtosEggInfo(egg_info):
+    def run(self):
+        # 1. Build the protobufs
+        BuildProtosCommand.run(self)
+        # 2. Run the installation
+        egg_info.run(self)
+
+
+setup(
+    cmdclass={
+        "build_package_protos": BuildProtosCommand,
+        "install": BuildProtosInstall,
+        "develop": BuildProtosDevelop,
+        "egg_info": BuildProtosEggInfo,
+        "clean": CleanFilesCommand,
+    }
+)

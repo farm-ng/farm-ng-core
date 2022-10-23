f44083dd943d7241e6465bdc9adc95a0e743d582
Ethan Rublee <ethan.rublee@gmail.com>
2022-10-23 09:53:19 -0700

    import bootstrap magic.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 # hack for bootstrapping and include farm_ng.core.package from it's setup.py
 import pathlib
 from pathlib import Path
+import sys
 
 from setuptools import Command
 from setuptools.command.develop import develop
@@ -10,7 +11,7 @@ from setuptools.command.egg_info import egg_info
 from setuptools.command.install import install
 from importlib.machinery import SourceFileLoader
 
-package_py = pathlib.Path(__file__).parent.resolve() / "py/farm_ng/core/package.py"
+package_py = pathlib.Path(__file__).parent.resolve() / "py/farm_ng/core/"
 
 
 class BuildProtosCommand(Command):
@@ -23,7 +24,9 @@ class BuildProtosCommand(Command):
         pass
 
     def run(self):
-        package = SourceFileLoader("farm_ng.core.package", package_py).load_module()
+        sys.path.append(str(package_py))
+        import package  # = SourceFileLoader("farm_ng.core.package", package_py).load_module()
+
         package.build_package_protos(proto_root=Path("protos"), package_root=Path("py"))
 
 
@@ -37,7 +40,9 @@ class CleanFilesCommand(Command):
         pass
 
     def run(self):
-        package = SourceFileLoader("farm_ng.core.package", package_py).load_module()
+        sys.path.append(str(package_py))
+        import package  # = SourceFileLoader("farm_ng.core.package", package_py).load_module()
+
         package.clean_protos(package_root=Path("py"))
 
 

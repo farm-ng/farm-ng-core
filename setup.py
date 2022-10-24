8d246ce570836a78e76755be71fb67039202f46e
Ethan Rublee <ethan.rublee@gmail.com>
2022-10-24 13:35:39 -0700

    Centralize python packaging logic. (#35)

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python3
+from setuptools import setup
+
+# hack for bootstrapping and include farm_ng.core.package from it's setup.py
+import pathlib
 from pathlib import Path
+import sys
 
 from setuptools import Command
-from setuptools import setup
 from setuptools.command.develop import develop
 from setuptools.command.egg_info import egg_info
 from setuptools.command.install import install
+from importlib.machinery import SourceFileLoader
 
-import shutil
+package_py = pathlib.Path(__file__).parent.resolve() / "py/farm_ng/core/"
 
 
 class BuildProtosCommand(Command):
@@ -20,27 +24,10 @@ class BuildProtosCommand(Command):
         pass
 
     def run(self):
-        from grpc_tools import command
+        sys.path.insert(0, str(package_py))
+        import package
 
-        proto_files_root = Path("protos")
-        command.build_package_protos(proto_files_root)
-
-        for proto_def in proto_files_root.rglob("*.proto"):
-            proto_def_new = Path("py", *proto_def.parts[1:])
-            shutil.copy(proto_def, proto_def_new)
-
-        for proto_file in proto_files_root.rglob("*_pb2*.py"):
-            proto_file_new = Path("py", *proto_file.parts[1:])
-            if not proto_file_new.exists():
-                proto_file.rename(proto_file_new)
-            if proto_file.exists():
-                proto_file.unlink()
-        for proto_file in proto_files_root.rglob("*_pb2*.pyi"):
-            proto_file_new = Path("py", *proto_file.parts[1:])
-            if not proto_file_new.exists():
-                proto_file.rename(proto_file_new)
-            if proto_file.exists():
-                proto_file.unlink()
+        package.build_package_protos(proto_root=Path("protos"), package_root=Path("py"))
 
 
 class CleanFilesCommand(Command):
@@ -53,13 +40,10 @@ class CleanFilesCommand(Command):
         pass
 
     def run(self):
-        proto_files_root = Path("py/farm_ng")
-        for proto_def in proto_files_root.rglob("*.proto"):
-            assert proto_def.unlink() is None
-        for proto_file in proto_files_root.rglob("*_pb2*.py"):
-            assert proto_file.unlink() is None
-        for proto_file in proto_files_root.rglob("*_pb2*.pyi"):
-            assert proto_file.unlink() is None
+        sys.path.insert(0, str(package_py))
+        import package
+
+        package.clean_protos(package_root=Path("py"))
 
 
 class BuildProtosInstall(install):
@@ -95,5 +79,5 @@ setup(
         "develop": BuildProtosDevelop,
         "egg_info": BuildProtosEggInfo,
         "clean": CleanFilesCommand,
-    },
+    }
 )

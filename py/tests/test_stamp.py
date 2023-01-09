8f62d559fc0f0e502eb48cc708a2d8ea6d9e9dec
Kyle Coble <kwc57@cornell.edu>
2023-01-09 11:23:57 -0500

    Unit test for system clock

--- py/tests/test_stamp.py
+++ py/tests/test_stamp.py
@@ -1,5 +1,5 @@
 from farm_ng.core import timestamp_pb2
-from farm_ng.core.stamp import get_monotonic_now
+from farm_ng.core.stamp import get_monotonic_now, get_system_clock_now
 
 
 def test_monotonic():
@@ -8,3 +8,11 @@ def test_monotonic():
     assert isinstance(stamp.stamp, float)
     assert "monotonic" in stamp.clock_name
     assert stamp.semantics == "test/monotonic"
+
+
+def test_system_clock():
+    stamp = get_system_clock_now(semantics="test/system_clock")
+    assert isinstance(stamp, timestamp_pb2.Timestamp)
+    assert isinstance(stamp.stamp, float)
+    assert "system_clock" in stamp.clock_name
+    assert stamp.semantics == "test/system_clock"

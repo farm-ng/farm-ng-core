6014319338aa8aec4e56172ace74b9d20f5e3e99
Kyle Coble <53625197+Hackerman342@users.noreply.github.com>
2023-07-19 10:22:58 -0400

    Timestamps refactor (farm-ng-core) (#92)

--- py/farm_ng/core/stamp.py
+++ py/farm_ng/core/stamp.py
@@ -1,6 +1,32 @@
 import time
+from dataclasses import dataclass
 from farm_ng.core.timestamp_pb2 import Timestamp
 from farm_ng.core.uri import get_host_name
+
+
+@dataclass
+class StampSemantics:
+    """
+    Recommended semantics for timestamp_pb2.Timestamp timestamps.
+
+    Example usage for when a device driver (service) receives a message from a device (e.g., a camera):
+        stamp = get_monotonic_now(StampSemantics.DRIVER_RECEIVE)
+    """
+
+    # When a gRPC client receives a message from a gRPC server.
+    CLIENT_RECEIVE = "client/receive"
+    # When a gRPC client sends a message to a gRPC server.
+    CLIENT_SEND = "client/send"
+    # When a device (e.g., camera) takes a measurement.
+    DEVICE_SAMPLE = "device/sample"
+    # When a device driver (gRPC service) receives a measurement/message from a device.
+    DRIVER_RECEIVE = "driver/receive"
+    # When a device driver (gRPC service) sends a message to a gRPC client.
+    DRIVER_SEND = "driver/send"
+    # When a device driver (gRPC service) sends a message to a device.
+    DRIVER_TRANSMIT = "driver/transmit"
+    # When a message is written to a file on disk.
+    FILE_WRITE = "file/write"
 
 
 def get_monotonic_now(semantics: str) -> Timestamp:

92b8815e694325cc12923682a8982a3de0034dbe
Kyle Coble <53625197+Hackerman342@users.noreply.github.com>
2023-08-18 13:12:52 -0400

    Add clock_type to get_stamp_by_semantics (#111)

--- py/tests/test_stamp.py
+++ py/tests/test_stamp.py
@@ -3,7 +3,7 @@ from farm_ng.core.event_pb2 import Event
 from farm_ng.core.stamp import (
     StampSemantics,
     get_monotonic_now,
-    get_stamp_by_semantics,
+    get_stamp_by_semantics_and_clock_type,
     get_system_clock_now,
     timestamp_from_monotonic,
 )
@@ -25,15 +25,53 @@ def test_system_clock():
     assert stamp.semantics == "test/system_clock"
 
 
-def test_get_semantics():
+def test_filter_stamp():
     timestamps = [
         timestamp_from_monotonic(semantics=StampSemantics.DRIVER_RECEIVE, stamp=1.23),
         timestamp_from_monotonic(semantics=StampSemantics.SERVICE_SEND, stamp=2.34),
+        get_system_clock_now(semantics=StampSemantics.SERVICE_SEND),
         timestamp_from_monotonic(semantics=StampSemantics.CLIENT_RECEIVE, stamp=3.45),
+        get_system_clock_now(semantics=StampSemantics.CLIENT_RECEIVE),
     ]
     event = Event(timestamps=timestamps)
 
-    assert get_stamp_by_semantics(event, StampSemantics.DRIVER_RECEIVE) == 1.23
-    assert get_stamp_by_semantics(event, StampSemantics.SERVICE_SEND) == 2.34
-    assert get_stamp_by_semantics(event, StampSemantics.CLIENT_RECEIVE) == 3.45
-    assert get_stamp_by_semantics(event, StampSemantics.DRIVER_TRANSMIT) is None
+    assert (
+        get_stamp_by_semantics_and_clock_type(
+            event,
+            StampSemantics.DRIVER_RECEIVE,
+            "monotonic",
+        )
+        == 1.23
+    )
+    assert (
+        get_stamp_by_semantics_and_clock_type(
+            event,
+            StampSemantics.SERVICE_SEND,
+            "monotonic",
+        )
+        == 2.34
+    )
+    assert (
+        get_stamp_by_semantics_and_clock_type(
+            event,
+            StampSemantics.CLIENT_RECEIVE,
+            "monotonic",
+        )
+        == 3.45
+    )
+    assert (
+        get_stamp_by_semantics_and_clock_type(
+            event,
+            StampSemantics.DRIVER_TRANSMIT,
+            "monotonic",
+        )
+        is None
+    )
+    assert (
+        get_stamp_by_semantics_and_clock_type(
+            event,
+            StampSemantics.SERVICE_RECEIVE,
+            "system_clock",
+        )
+        is None
+    )

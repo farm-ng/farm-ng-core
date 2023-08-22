d75096bd58de2e5ba5e43f066c04033779142504
Hauke Strasdat <strasdat@gmail.com>
2023-08-21 21:37:14 -0700

    refactor: polish script and structure

---
+++ cpp/sophus/README.md
@@ -0,0 +1,1 @@
+sophus2 forked form https://github.com/strasdat/Sophus/commits/sophus2 27056f4e8aca3d36ffe6796149df5388c28767fc
---
+++ scripts/install_deps_ubuntu.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -x # echo on
+set -e # exit on error
+
+cmake --version
+
+sudo apt-get -qq update
+# Please keep this list sorted.
+sudo apt-get -y install \
+     build-essential \
+     ccache \
+     clang \
+     clang-tidy \
+     cmake \
+     curl \
+     gfortran \
+     libassimp-dev \
+     libatlas-base-dev \
+     libavcodec-dev \
+     libavdevice-dev \
+     libavformat-dev \
+     libavutil-dev \
+     libc++-dev \
+     libcrypto++-dev \
+     libegl1-mesa-dev \
+     libglew-dev \
+     libglm-dev \
+     libgoogle-glog-dev \
+     libgtest-dev \
+     libopencv-dev \
+     libssl-dev \
+     libstdc++-10-dev \
+     libsuitesparse-dev \
+     libswscale-dev \
+     libtiff-dev \
+     ninja-build \
+     python3-dev \
+     python3.8-venv \
+     && sudo apt-get clean

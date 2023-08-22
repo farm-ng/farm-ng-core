9bd07a6af4de34dc2595026d7f289d0c1976f76a
Ethan Rublee <ethan.rublee@gmail.com>
2023-08-22 14:55:54 -0700

    Fix pose_tree.

--- py/farm_ng/core/pose_tree.py
+++ py/farm_ng/core/pose_tree.py
@@ -15,8 +15,8 @@ if TYPE_CHECKING:
 
 def find_transform(
     pose_tree: pose_pb2.PoseTree,
-    frame_A: str,
-    frame_B: str,
+    frame_a: str,
+    frame_b: str,
 ) -> lie_pb2.Isometry3F64 | None:
     """Find all poses in a PoseTree with a given frame_A and frame_B.
 
@@ -24,17 +24,17 @@ def find_transform(
 
     Args:
         pose_tree (PoseTree): The PoseTree proto object to search within.
-        frame_A (str): The desired frame_A value.
-        frame_B (str): The desired frame_B value.
+        frame_a (str): The desired frame_a value.
+        frame_b (str): The desired frame_b value.
 
     Returns:
         Pose: Pose objects that match the criteria.
     """
     pose: pose_pb2.Pose
     for pose in pose_tree.poses:
-        if pose.frame_A == frame_A and pose.frame_B == frame_B:
+        if pose.frame_a == frame_a and pose.frame_b == frame_b:
             return pose.B_from_A
-        if pose.frame_A == frame_B and pose.frame_B == frame_A:
+        if pose.frame_a == frame_b and pose.frame_b == frame_a:
             print("NOT IMPLEMENTED, please swap your frames")
             return None
     return None

02c923b70eb74760a71c6e5486afb8673b0044fb
Ian Sherman <ian.j.sherman@gmail.com>
2023-12-26 11:39:43 -0800

    Add find_dependency to config.cmake

--- cmake/farm_ng_core-config.cmake
+++ cmake/farm_ng_core-config.cmake
@@ -1,1 +1,9 @@
+include(CMakeFindDependencyMacro)
+
+find_dependency(fmt REQUIRED)
+find_dependency(Eigen3 3.4.0 REQUIRED)
+find_dependency(Protobuf REQUIRED)
+find_dependency(gRPC REQUIRED)
+find_dependency(Boost REQUIRED COMPONENTS system)
+
 include(${CMAKE_CURRENT_LIST_DIR}/farm_ng_coreTargets.cmake)

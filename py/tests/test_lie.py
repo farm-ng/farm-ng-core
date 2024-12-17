d9e961ffe460a0485471d0978148ebea73479e8c
Patrick Mihelich <patrick@farm-ng.com>
2024-12-17 10:21:01 -0600

    More debug test output.

--- py/tests/test_lie.py
+++ py/tests/test_lie.py
@@ -147,7 +147,9 @@ def test_pose():
 
         # computes the error between frame_b of two respective poses
         # e.g. robot and robot now
-        err = ng.Pose3F64.error(world_from_robot, world_from_robot_now) * (1 / dt)
+        err_unscaled = ng.Pose3F64.error(world_from_robot, world_from_robot_now)
+        print("err_unscaled:", err_unscaled)
+        err = err_unscaled * (1 / dt)
         print("err:", err)
         print("tan:", world_from_robot_now.tangent_of_b_in_a)
         assert np.allclose(err, world_from_robot_now.tangent_of_b_in_a)
